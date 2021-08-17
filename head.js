002dcb69511f7bf15925f93808252964b34c652b
kyl graym <kylemaxgray@gmail.com>
2021-08-17 02:10:50 +0000

    finish assert equals and creat head

--- head.js
+++ head.js
@@ -9,3 +9,10 @@ const assertEqual = function (actual, expected) {
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+const head = function (array) {
+  return array[0];
+}
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
