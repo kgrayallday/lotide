9367bdb5823c217a42cfbe83629931f376491e1a
kyl graym <kylemaxgray@gmail.com>
2021-08-19 15:18:35 +0000

    adding comment

--- eqObjects.js
+++ eqObjects.js
@@ -58,7 +58,7 @@ assertEqual(eqObjects(ab, abc), false); // => false
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
+assertEqual(eqObjects(cd, dc), true); // => true (SHOULD BE TRUE?)
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false); // => false
