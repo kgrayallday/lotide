ae2dba1e4a54d2ad79cb644c6f4335e14c671e4d
kyl graym <kylemaxgray@gmail.com>
2021-08-24 04:47:39 +0000

    refactoring tail and middle

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,22 @@
-const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertEqual(tail(words), ["Lighthouse", "Labs"]); // original array should still have 3 elements!
+// assertEqual(tail(words), ["Lighthouse", "Labs"]); // original array should still have 3 elements!
+
+describe("#tail", () =>{
+
+  it('returns ["Lighthouse", "Labs"] for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(words), ["Lighthouse", "Labs"]);
+  });
+
+  it('returns [2, 3] for [1, 2, 3]', () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+} )
